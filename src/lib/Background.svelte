79d6633aacd0957e78a794189c523be83180df40
MrExplode <me@sunstorm.rocks>
2023-10-05 20:43:35 +0200

    feat: throw out vanta into the thrash
    changed my mind, it doesn't look good.

--- src/lib/Background.svelte
+++ src/lib/Background.svelte
@@ -1,30 +1,7 @@
-<script>
-	import * as THREE from 'three'
-	import WAVES from 'vanta/dist/vanta.waves.min'
-
-	function vanta(node) {
-		const anim = WAVES({
-			el: node,
-			THREE: THREE,
-			mouseControls: true,
-			touchControls: false,
-			gyroControls: true,
-			minHeight: 200.0,
-			minWidth: 200.0,
-			scale: 1.0,
-			scaleMobile: 1.0,
-			color: 0x161616,
-			zoom: 1.0,
-			waveSpeed: 0.5
-		})
-		return {
-			destroy() {
-				anim.destroy()
-			}
-		}
-	}
+<script lang="ts">
+	// todo: some good background maybe
 </script>
 
-<div use:vanta class="w-screen h-screen">
+<div class="w-screen h-screen">
 	<slot />
 </div>
