ab953bf4487f956475e642a139f545f032eff29f
MrExplode <me@sunstorm.rocks>
2023-10-11 22:08:02 +0200

    fix: scoreboard desync

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	import Background from '$lib/Background.svelte'
 	import '$lib/app.css'
+	import { registerScoreboard } from '$lib/scoreboard'
+	import { onMount } from 'svelte'
+
+	onMount(() => registerScoreboard())
 </script>
 
 <svelte:head>
